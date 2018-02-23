5906a2212aa8947939253c7e3df85c22edf9b023
yash-itt <36620263+yash-itt@users.noreply.github.com>
2018-02-23 20:25:49 +0530

    build.sh changed

--- devops/scripts/build.sh
+++ devops/scripts/build.sh
@@ -8,8 +8,8 @@ then
         sudo docker stop $isContainerRunning
         sudo docker rm $isContainerRunning
 fi
-sudo docker build --build-arg tag=$webContainerTag -f /home/ubuntu/dockerFiles/web/dockerfile .
-sudo docker build --rm --pull=true --build-arg tag=$webContainerTag -t yashittdocker/adpq-web -f /home/ubuntu/dockerFiles/web/dockerfile .
+sudo docker build --build-arg tag=$webContainerTag -f devops/dockerfile/web/dockerfile .
+sudo docker build --rm --pull=true --build-arg tag=$webContainerTag -t yashittdocker/adpq-web -f devops/dockerfile/web/dockerfile .
 sudo docker run -d --name adpq-web -p 80:8080 yashittdocker/adpq-web:$webContainerTag
 
 isContainerRunning=$(sudo docker ps -q -f "name=adpq-mongo")
@@ -18,6 +18,6 @@ then
         sudo docker stop $isContainerRunning
         sudo docker rm $isContainerRunning
 fi
-sudo docker build --build-arg tag=$mongoContainerTag -f /home/ubuntu/dockerFiles/mongo/dockerfile .
-sudo docker build --rm --pull=true --build-arg tag=$mongoContainerTag -t yashittdocker/adpq-mongo -f /home/ubuntu/dockerFiles/mongo/dockerfile .
+sudo docker build --build-arg tag=$mongoContainerTag -f devops/dockerfile/mongo/dockerfile .
+sudo docker build --rm --pull=true --build-arg tag=$mongoContainerTag -t yashittdocker/adpq-mongo -f devops/dockerfile/mongo/dockerfile .
 sudo docker run -d --name adpq-mongo -p 27017:27017 yashittdocker/adpq-mongo:$mongoContainerTag
